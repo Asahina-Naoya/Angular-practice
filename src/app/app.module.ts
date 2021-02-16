40fb2302440b2d5394d1e0522589cb0140dea90c
Asahina-Naoya <hina_ssb@outlook.jp>
2021-02-16 16:34:02 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { IndexComponent } from './index/index.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
